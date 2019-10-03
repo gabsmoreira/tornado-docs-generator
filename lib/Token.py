25f0c46459b2bd8e8fb4d31488c4d4d3b8a4566d
gabsmoreira <gabrielfm2@al.insper.edu.br>
2019-10-03 16:55:35 -0300

    FIX - verify if ENDLINE before status code

--- lib/Token.py
+++ lib/Token.py
@@ -5,21 +5,23 @@ class Token:
     def __init__(self, token):
         self.OPS = ['+', '-', '/', '*']
         # self.TOKEN_TYPES = ['OP', 'INT', 'VAR', 'PRINT']
-        self.value = token
+        self.value = str(token)
         self.type = self.define_type(token)
+        # print(self.type)
 
     def define_type(self, token):
+        token = str(token)
         if token[1:].upper() in TITLES:
             self.value = token[1:]
             return 'TITLE'
+        elif token[-1] == '.':
+            self.value = token[:-1]
+            return 'ENDLINE'
         elif token[:-1].upper() in SUB_TITLES or token[:-1].isdigit():
             self.value = token[:-1]
             return 'SUB'
         elif token == 'EOF':
             return token
-        elif token[-1] == '.':
-            self.value = token[:-1]
-            return 'ENDLINE'
         elif token == '-':
             return 'SEPARATOR'
         elif token == 'file:':
